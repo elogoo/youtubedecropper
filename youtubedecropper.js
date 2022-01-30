2704de313507d26ea2c16a3b75e5cff57e5fd949
elogoo <elogo@protonmail.com>
2022-01-30 17:48:57 +0100

    it's now searching for a regex instead of a string to find the right part of the url to slice

--- youtubedecropper.js
+++ youtubedecropper.js
@@ -1,9 +1,11 @@
-console.log("doing it");
+console.log("doing it now");
 url = location.href;
 console.log(url);
-console.log(url.match("="));
-if(url.match("=")){
-    var index = url.indexOf("640");
-    url = url.slice(0,index + 3);
+console.log(url.match(/[a-z]\d\d\d/));
+stringmatch = url.match(/[a-z]\d\d\d/);
+if(url.match("crop")){
+    console.log("doing it for real");
+    var index = url.indexOf(stringmatch);
+    url = url.slice(0,index + 4);
     location.href = url;
 }
